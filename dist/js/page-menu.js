6eb47942546f1b0ff387601cc2cab13c0030e7a4
Tim Chumbash <posseslash@gmail.com>
2020-01-31 10:09:46 +0100

    page-menu - sticky

---
+++ dist/js/page-menu.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let pageMenu = document.querySelector('.page-menu');
+let sideContent = document.querySelector('.side-content-container');
+
+
+document.addEventListener('scroll', function () {
+    let objRect = pageMenu.getBoundingClientRect().top;
+    let viewport = document.body.clientWidth;
+    console.log(objRect);
+    console.log(viewport);
+    if (viewport > 1280) {
+        if (objRect < 200) {
+            pageMenu.classList.add('page-menu--fixed')
+        } else if (objRect > 200) {
+            pageMenu.classList.remove('page-menu--fixed')
+        }
+    }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ gulp.task("css", function () {
         ]))
         .pipe(sourcemap.write("."))
         .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("../forrest-mix-python/hotel/static/css"))
         .pipe(server.stream());
 });
 
