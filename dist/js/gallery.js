f52d7cefd4fbde8d8e6e3a82bbaee124f14ad64e
Tim Chumbash <posseslash@gmail.com>
2020-02-28 17:09:07 +0100

    gallery show/hide

---
+++ dist/js/gallery.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let btns = document.querySelectorAll('.js-gallery-btn'),
+    galleryClass = 'gallery',
+    galleryBtnCloseClass = 'gallery__close',
+    galleryHideClass = 'gallery--hidden';
+
+
+let toggleGallery = function(gallery, section, on = true){
+    if (on) {
+        gallery.classList.remove(galleryHideClass);
+        section.style.zIndex = '999';
+    } else {
+        gallery.classList.add(galleryHideClass);
+        section.style.zIndex = '';
+    }
+};
+
+btns.forEach(function (btn) {
+    let
+        section = btn.closest('section'),
+        gallery = section.querySelector('.' + galleryClass),
+        closeBtn = gallery.querySelector('.' + galleryBtnCloseClass);
+    console.log(closeBtn);
+    console.log(gallery);
+    console.log(section);
+    btn.addEventListener('click', function(){
+        toggleGallery(gallery, section)
+    });
+    closeBtn.addEventListener('click', function close(){
+        toggleGallery(gallery, section, false);
+        closeBtn.removeEventListener('click', close)
+    })
+});
--- dist/js/parallax.js
+++ dist/js/parallax.js
@@ -3,26 +3,5 @@
 let sectionHeight = 610; // height of an area text floats within
 let cursives = document.querySelectorAll('.main-card__cursive-aside');
 
-cursives.forEach(function(el){
-    let textEl = el.querySelector('.cursive-aside-title');
-    let height = textEl.clientWidth;
-    console.log(textEl);
-    let waypoint = new Waypoint({
-        element: el,
-        handler: function(direction) {
-            console.log(el);
-            console.log("trigger");
-        },
-        offset: height*3
-    });
-    let waypoint2 = new Waypoint({
-        element: el,
-        handler: function(direction) {
-            console.log(el);
-            console.log("trigger2");
-        },
-        offset: -height*3
-    });
-});
 
 
